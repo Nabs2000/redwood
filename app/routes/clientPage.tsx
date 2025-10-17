ab0d3a1b580201fc204f74245d13a40dbbfab907
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-10-16 23:06:42 -0700

    Render basic referral form (not sending emails yet)

--- app/routes/clientPage.tsx
+++ app/routes/clientPage.tsx
@@ -3,6 +3,7 @@ import type { Route } from "./+types/clientPage";
 import { getDoc, doc } from "firebase/firestore";
 import { getFirestore } from "firebase/firestore";
 import type { Client } from "~/types/client.types";
+import { useState } from "react";
 
 const db = getFirestore();
 
@@ -14,15 +15,27 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export async function loader({ params }: Route.LoaderArgs) {
-    const { clientId } = params;
-    const userDoc = await getDoc(doc(db, "clients", clientId));
-    if (!userDoc.exists()) {
-        throw new Response("User not found", { status: 404 });
-    }
-    return userDoc.data();
+  const { clientId } = params;
+  const userDoc = await getDoc(doc(db, "clients", clientId));
+  if (!userDoc.exists()) {
+    throw new Response("User not found", { status: 404 });
+  }
+  return userDoc.data();
 }
-    
 
-export default function ClientPage({ loaderData }: {loaderData: Client}) {
-    return <div>Hello {loaderData.firstName}!</div>;
+export default function ClientPage({ loaderData }: { loaderData: Client }) {
+  const [showReferralForm, setShowReferralForm] = useState(false);
+  return (
+    <div>
+      <h1>Hello {loaderData.firstName}!</h1>
+      {showReferralForm ? (
+        <button onClick={() => setShowReferralForm(false)}>Close</button>
+      ) : (
+        <button onClick={() => setShowReferralForm(true)}>
+          Show Referral Form
+        </button>
+      )}
+      {showReferralForm && <ReferralForm client={loaderData} />}
+    </div>
+  );
 }
