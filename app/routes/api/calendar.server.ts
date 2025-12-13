7741ec07c1876435a365d45a7970331dce51c087
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-12-12 22:42:43 -0800

    Uses access token to authenticate with Google Calendar API

---
+++ app/routes/api/calendar.server.ts
@@ -0,0 +1,14 @@
+import { google } from "googleapis";
+
+export const getCalendarList = async (accessToken: string) => {
+  // Create an OAuth2 client with the access token
+  const oauth2Client = new google.auth.OAuth2();
+  oauth2Client.setCredentials({ access_token: accessToken });
+
+  const calendar = google.calendar({
+    version: "v3",
+    auth: oauth2Client,
+  });
+
+  return calendar.calendarList.list();
+};
